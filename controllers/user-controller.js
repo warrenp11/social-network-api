1dc164fa4e8dec305042973adc87cae9005f78f7
Peter <warrenp11@gmail.com>
2022-01-13 08:48:47 -0500

    skeleton user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,3 +1,28 @@
 const { User } = require("../models");
 
 // User controllers here
+const userController = {
+    
+    // get all users
+    
+    // get user by id
+    
+    // create user
+    
+    // update user
+    
+    // delete user (BONUS: remove user's associated thoughts when deleted)
+    
+}
+
+
+
+
+
+
+
+// example data
+// {
+//     "username": "lernantino",
+//     "email": "lernantino@gmail.com"
+//   }
